9db12b6a57612dcfa11817089931c67f2474d077
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-11-17 23:03:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+
+        while (t-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int[] a = new int[input.length]; // Change long to int
+
+            for (int i = 0; i < input.length; i++) {
+                a[i] = Integer.parseInt(
+                    input[i]); // Change Long.parseLong to Integer.parseInt
+            }
+
+            Solution ob = new Solution();
+            System.out.println(
+                ob.minCost(a)); // Ensure minCost accepts int[] as argument
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return the minimum cost of connecting the ropes.
+    public int minCost(int[] arr) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0;i<arr.length;i++){
+            pq.add(arr[i]);
+        }
+        
+        int cost = 0;
+        while(pq.size() > 1){
+            int min = pq.remove();
+            int min2 = pq.remove();
+            cost += min + min2;
+            pq.add(min + min2);
+        }
+        
+        return cost;
+    }
+}
