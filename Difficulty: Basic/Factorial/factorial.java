3dca57612aa9164d5aa3fbf076651bf2fbedc340
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-12-05 17:51:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -21,7 +21,7 @@ class GFG {
 class Solution {
     static int factorial(int n) {
         // code here
-        if(n == 0) return 1;
+        if(n == 0 || n == 1) return 1;
         
         return n * factorial(n-1);
     }
