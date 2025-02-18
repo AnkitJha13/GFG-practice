38db6436783e45a1e8710d46e9d48c92bf1fc3d6
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-02-18 23:19:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reorder List/reorder-list.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/* Following is the Linked list node structure */
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    void reorderlist(Node head) {
+        // Your code here
+        if(head == null) return ;
+        Node slow = head, fast = head;
+        
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        Node second = slow.next;
+        slow.next = null;
+        Node prev = null;
+        
+        while(second!=null){
+            Node temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+        
+        Node first = head;
+        second = prev;
+        
+        while(second!=null){
+            Node temp1 = first.next, temp2 = second.next;
+            
+            first.next = second;
+            second.next = temp1;
+            
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); i++) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+            new Solution().reorderlist(head);
+
+            printList(head);
+        }
+    }
+}
+// } Driver Code Ends
