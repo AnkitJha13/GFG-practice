e3c4ba0e0a7b8f5973d43282dc24a28fad5ac803
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-03-03 10:21:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] S = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(S[0]);
+            S = br.readLine().trim().split(" ");
+            int E = Integer.parseInt(S[0]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<Integer>());
+            }
+            for (int i = 0; i < E; i++) {
+                String[] s = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isBipartite(adj);
+            if (ans)
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public boolean isBipartite(ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        int n = adj.size();
+        int color[] = new int[n];
+        Arrays.fill(color, -1);
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(color[i] == -1){
+                color[i] = 0;
+                q.add(i);
+                
+                while(!q.isEmpty()){
+                    int node = q.remove();
+                    
+                    for(int neighbor : adj.get(node)){
+                        if(color[neighbor] == -1){
+                            color[neighbor] = 1 - color[node];
+                            q.add(neighbor);
+                        }
+                        else if(color[neighbor] == color[node]){
+                            return false;
+                        }
+                        else if(color[neighbor] != color[node]){
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
