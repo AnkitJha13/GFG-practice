3d1e0cd6a4dab0df0ceab2902347b0db9282cb2a
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-08-22 12:51:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.java
@@ -0,0 +1,43 @@
+// User function Template for Java
+
+class Solution {
+    void rearrange(ArrayList<Integer> arr) {
+        // code here
+        
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        
+        for(int num : arr){
+            if(num >= 0){
+                pos.add(num);
+            }
+            else{
+                neg.add(num);
+            }
+        }
+        
+        arr.clear();
+        
+        int i=0, j=0;
+        
+        while(i < pos.size() && j < neg.size()){
+            arr.add(pos.get(i));
+            i++;
+            
+            arr.add(neg.get(j));
+            j++;
+        }
+        
+        while(i < pos.size()){
+            arr.add(pos.get(i));
+            i++;
+        }
+        
+        while(j < neg.size()){
+            arr.add(neg.get(j));
+            j++;
+        }
+        
+        
+    }
+}
