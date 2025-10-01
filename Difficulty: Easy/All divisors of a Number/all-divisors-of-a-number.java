712488a2c1086436ff408278c391fd5abb3bdee2
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-10-01 17:38:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.java
@@ -3,12 +3,14 @@ class Solution {
         // code here
         List<Integer> divisors = new ArrayList<>();
         
-        // Find divisors up to sqrt(n)
+        // Loop only till sqrt(n), because divisors come in pairs (i, n/i)
+        // Example: for 20 -> (1,20), (2,10), (4,5). Beyond sqrt(n), pairs repeat.
         for (int i = 1; i * i <= n; i++) {
             if (n % i == 0) {
                 divisors.add(i);
-                if (i != n / i) { // To avoid adding square root twice
-                    divisors.add(n / i);
+                if (i != n / i) { // If n is not a perfect square, add the paired divisor (n / i)
+                                  // Example: for n = 36, when i = 6 → n/i = 6, avoid duplicate.
+                   divisors.add(n / i);
                 }
             }
         }
